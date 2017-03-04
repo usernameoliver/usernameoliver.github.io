2980bacd1a8557e6c21a05466c04e8e5fac7ad33
OliverSurfaceBook <oliverhaobit@gmail.com>
2017-03-04 16:38:14 -0500

    re-organize file structures

--- myWebsite/index.js
+++
@@ -1,5 +0,0 @@
-function kosbie_face_event_handler(){
-	alert("ouch")
-}
-$(".kosbiesface").click(kosbie_face_event_handler)
-$(document).ready()
---
+++ myWebsite/js/index.js
@@ -0,0 +1,5 @@
+function kosbie_face_event_handler(){
+	alert("ouch")
+}
+$(".kosbiesface").click(kosbie_face_event_handler)
+$(document).ready()
